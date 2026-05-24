5ee0a880e7bea095236671d5d81d0479962ea71d
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-05-24 21:55:43 +0530

    Create vectors100r.v

---
+++ Verilog/More_Verilog_features/vectors100r.v
@@ -0,0 +1,16 @@
+/*
+Given a 100-bit input vector [99:0], reverse its bit ordering.
+*/
+
+module top_module( 
+    input [99:0] in,
+    output reg [99:0] out
+);
+    
+    always @(*) begin
+        for(integer i = 0; i < 100; i = i + 1) begin
+            out[i] = in[99 - i];
+        end
+    end
+               
+endmodule
