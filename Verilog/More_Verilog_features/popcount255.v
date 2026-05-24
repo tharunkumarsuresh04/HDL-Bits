b5142112a14e4f52da655ce3dd0322a4d3116222
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-05-24 21:56:38 +0530

    Create popcount255.v

---
+++ Verilog/More_Verilog_features/popcount255.v
@@ -0,0 +1,17 @@
+/*
+A "population count" circuit counts the number of '1's in an input vector. Build a population count circuit for a 255-bit input vector.
+*/
+
+module top_module( 
+    input [254:0] in,
+    output reg [7:0] out );
+    
+    
+    always @(*) begin 
+        out = 8'd0;
+        for(integer i = 0; i < 255; i = i + 1) begin 
+            out = out + in[i];
+        end
+    end
+
+endmodule
