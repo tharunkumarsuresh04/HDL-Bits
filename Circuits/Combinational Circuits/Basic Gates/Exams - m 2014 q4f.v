eb88717afe9c775893714531d914d553f5001376
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-05-26 20:27:17 +0530

    Create Exams - m 2014 q4f.v

---
+++ Circuits/Combinational Circuits/Basic Gates/Exams - m 2014 q4f.v
@@ -0,0 +1,13 @@
+/* 
+Implement the following function: 
+            Y = A & (~B) ;
+*/
+
+module top_module (
+    input in1,
+    input in2,
+    output out);
+
+	assign out = in1 & ~in2;
+
+endmodule
