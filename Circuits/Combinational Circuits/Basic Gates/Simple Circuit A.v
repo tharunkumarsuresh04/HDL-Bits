944fd97cbd5b45d1cd3c31524e7fafa61f5fd806
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-05-26 20:34:52 +0530

    Create Simple Circuit A.v

---
+++ Circuits/Combinational Circuits/Basic Gates/Simple Circuit A.v
@@ -0,0 +1,11 @@
+/*
+Taken from 2015 midterm question 4
+
+Module A is supposed to implement the function z = (x^y) & x. Implement this module.
+*/
+
+module top_module (input x, input y, output z);
+   
+    assign z = (x ^ y) & x;
+    
+endmodule
