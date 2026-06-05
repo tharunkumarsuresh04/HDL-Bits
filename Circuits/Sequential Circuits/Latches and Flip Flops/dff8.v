5d585100fecbff2da26887c927ca337a332a24eb
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-06-05 06:33:47 +0530

    Add 8 D flip-flops triggered by clk

---
+++ Circuits/Sequential Circuits/Latches and Flip Flops/dff8.v
@@ -0,0 +1,16 @@
+/*
+Create 8 D flip-flops. All DFFs should be triggered by the positive edge of clk.
+*/
+
+module top_module (
+    input clk,
+    input [7:0] d,
+    output [7:0] q
+);
+    
+    always @(posedge clk) begin
+        for (integer i = 0; i < 8; i = i + 1) 
+            q[i] <= d[i];
+    end
+
+endmodule
