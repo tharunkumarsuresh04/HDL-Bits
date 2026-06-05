2cfd0382c0acdb5bf94f29cdbda7e02386004149
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-06-05 06:34:31 +0530

    Add 8 D flip-flops with synchronous reset

---
+++ Circuits/Sequential Circuits/Latches and Flip Flops/dff8r.v
@@ -0,0 +1,20 @@
+/*
+Create 8 D flip-flops with active high synchronous reset. All DFFs should be triggered by the positive edge of clk.
+*/
+
+module top_module (
+    input clk,
+    input reset,            // Synchronous reset
+    input [7:0] d,
+    output reg [7:0] q
+);
+    
+    always @(posedge clk) begin
+        if(reset) begin
+            q <= 0;
+        end else begin
+            q <= d;
+        end
+    end
+
+endmodule
