2d60e391baf7e1ff8ff0681be51f104f10febe26
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-06-07 21:38:46 +0530

    Create Exams - m 2014 q4d.v

---
+++ Circuits/Sequential Circuits/Latches and Flip Flops/Exams - m 2014 q4d.v
@@ -0,0 +1,18 @@
+
+
+
+module top_module (
+    input clk,
+    input in, 
+    output reg out);
+   
+    wire d;
+    assign d = in ^ out;
+  
+    
+    always @(posedge clk) begin 
+		out <= in ^ out;
+        
+    end
+
+endmodule
