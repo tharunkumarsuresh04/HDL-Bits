c965026d36cb9a988415eea4b813b6b338d40001
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-06-08 10:14:55 +0530

    Created count10.v

---
+++ Circuits/Sequential Circuits/Counters/count10.v
@@ -0,0 +1,22 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+        
+        if(reset) begin
+            q <= 4'd0;
+        end
+        else begin 
+            if (q == 4'd9) begin
+                q <= 4'd0;
+            end
+            else begin
+                q <= q + 1'b1;
+            end
+        end
+    end
+        
+
+endmodule
