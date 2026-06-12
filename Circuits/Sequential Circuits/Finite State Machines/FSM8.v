8ea5ec471baef2e0366e0c511eaf6f10e2616eea
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-06-12 09:23:01 +0530

    Created FSm8.v

---
+++ Circuits/Sequential Circuits/Finite State Machines/FSM8.v
@@ -0,0 +1,48 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out); //
+    
+    parameter A = 2'b00, B = 2'b01, C = 2'b10, D = 2'b11;
+    
+    reg[1:0] present_state, next_state;
+        
+    // State transition logic
+    
+    always @(*) begin
+        case (present_state)
+            A: begin
+                next_state = (in == 1'b1) ? B : A;
+            end
+            B: begin
+                next_state = (in == 1'b1) ? B : C;
+            end
+            C: begin
+                next_state = (in == 1'b1) ? D : A;
+            end
+            D: begin
+                next_state = (in == 1'b1) ? B : C;
+            end
+            default: begin
+                next_state = A;
+            end
+        endcase
+    end
+    
+    // State flip-flops with synchronous reset
+    
+    always @(posedge clk) begin
+        if(reset) begin
+            present_state <= A;
+        end
+        else begin
+            present_state <= next_state;
+        end
+    end
+
+    // Output logic
+    
+    assign out = (present_state == D);
+
+endmodule
