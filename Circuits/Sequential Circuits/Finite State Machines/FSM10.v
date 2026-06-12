167bd41fb213b3560b81b43e1d43a6f1c2fedd87
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-06-12 09:25:32 +0530

    Created FSM10.v

---
+++ Circuits/Sequential Circuits/Finite State Machines/FSM10.v
@@ -0,0 +1,52 @@
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    output walk_left,
+    output walk_right); //  
+
+    // parameter LEFT=0, RIGHT=1, ...
+    parameter LEFT = 1'b0; 
+    parameter RIGHT = 1'b1;
+    reg state, next_state;
+
+    always @(*) begin
+        // State transition logic
+        case (state) 
+            LEFT: begin
+                next_state = (bump_left) ? RIGHT : LEFT;
+            end
+            
+            RIGHT: begin
+                next_state = (bump_right) ? LEFT : RIGHT;
+            end
+            
+            default: begin
+                next_state = LEFT;
+            end
+            
+        endcase
+    end
+
+    always @(posedge clk, posedge areset) begin
+     // State flip-flops with asynchronous reset
+        if(areset) begin
+            state <= LEFT; 
+        end 
+        
+        else begin
+            state <= next_state;
+        end
+ 
+    end
+
+    // Output logic
+    // assign walk_left = (state == ...);
+    // assign walk_right = (state == ...);
+    assign walk_left = (state == LEFT);
+    
+    assign walk_right = (state == RIGHT);
+
+
+endmodule
