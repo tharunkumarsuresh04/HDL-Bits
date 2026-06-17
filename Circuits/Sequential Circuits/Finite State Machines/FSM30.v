05206823da0ec9d9ad68d3d9ff58dd239753237a
BHARATH KUMAR S <tharunkumar.suresh04@gmail.com>
2026-06-17 10:24:57 +0530

    Created FSM30.v

---
+++ Circuits/Sequential Circuits/Finite State Machines/FSM30.v
@@ -0,0 +1,33 @@
+module top_module (
+    input clk,
+    input reset,     // Synchronous active-high reset
+    input w,
+    output z
+);
+
+    parameter A=0, B=1, C=2, D=3, E=4, F=5;
+    reg [2:0] state, next_state;
+
+    // Combinational State Transition Logic
+    always @(*) begin
+        case(state)
+            A: next_state = w ? B : A; 
+            B: next_state = w ? C : D; 
+            C: next_state = w ? E : D; 
+            D: next_state = w ? F : A; 
+            E: next_state = w ? E : D; 
+            F: next_state = w ? C : D; 
+            default: next_state = A;
+        endcase
+    end
+
+    // Sequential State Memory
+    always @(posedge clk) begin
+        if (reset) state <= A;
+        else       state <= next_state;
+    end
+
+    // Continuous Assignment Output
+    assign z = (state == E) | (state == F);
+
+endmodule
